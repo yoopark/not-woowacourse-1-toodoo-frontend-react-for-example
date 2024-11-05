253d189caa0b6265cf5d9743970b3bdbfdc56bff
Yongjun Park <yopark@student.42seoul.kr>
2024-11-05 22:45:41 +0900

    chore: remove useless files

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
+import { App } from '@/App.tsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { Provider } from './components/ui/provider.tsx';
+import { Provider as ChakraUIProvider } from './components/ui/provider.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (rootElement === null) {
+  throw new Error('Root element not found');
+}
+
+if (rootElement.hasChildNodes()) {
+  throw new Error('Root element already has children');
+}
+
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <Provider>
+    <ChakraUIProvider>
       <App />
-    </Provider>
+    </ChakraUIProvider>
   </StrictMode>
 );
